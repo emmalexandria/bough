d109bc6b479060c4a5802c7dbb6e0efb96a5b4bd
Emma Jellema-Butler <emma.jellemabutler@gmail.com>
2025-09-20 10:11:15 +0200

    Switch to clap-help

--- bough/src/cli.rs
+++ bough/src/cli.rs
@@ -1,14 +1,35 @@
-use std::path::PathBuf;
+use std::{fmt::Display, path::PathBuf};
 
-use bough_lib::output::icons::IconType;
-use clap::{command, value_parser, Arg, ArgAction, ArgMatches, Command, ValueEnum};
+use bough_lib::icons::IconType;
+use clap::{command, ArgMatches, Command, CommandFactory, Parser, ValueEnum};
+use clap_help::Printer;
+use crossterm::style::Color;
 
-#[derive(ValueEnum, Clone, Copy)]
+#[derive(ValueEnum, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum IconArg {
     None,
     Nerd,
     Unicode,
     Emoji,
+}
+
+#[derive(ValueEnum, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum OutputFormat {
+    HTML,
+    Text,
+    ANSI,
+    Markdown,
+}
+
+impl Display for IconArg {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Self::None => "none",
+            Self::Nerd => "nerd",
+            Self::Unicode => "unicode",
+            Self::Emoji => "emoji",
+        })
+    }
 }
 
 impl Into<IconType> for IconArg {
@@ -22,77 +43,114 @@ impl Into<IconType> for IconArg {
     }
 }
 
-// I personally prefer the clap builder api, but having a struct of arguments is still nice
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Parser, Debug, Clone, PartialEq, Eq, Hash)]
+#[command(author, version, about, disable_help_flag = true)]
 pub struct Args {
+    #[arg(
+        long,
+        short,
+        default_value_t = 0,
+        help = "The depth to build the file tree to. 0 will build as much as possible."
+    )]
     pub depth: usize,
-    pub icons: IconType,
+    #[arg(long, short, default_value = "text")]
+    pub format: OutputFormat,
+    #[arg(long, short, default_value_t = IconArg::None, value_name="STYLE")]
+    pub icons: IconArg,
+    #[arg(long, short, value_name = "PATH", help = "Output to the given file.")]
+    pub out: Option<PathBuf>,
+    #[arg(
+        long,
+        short,
+        default_value_t = false,
+        help = "Copy the output to the system clipboard."
+    )]
+    pub copy: bool,
+    #[arg(default_value = "./", help = "The root path of the tree.")]
     pub path: PathBuf,
+    #[arg(long = "all", short, help = "Display hidden files in the output.")]
     pub all_files: bool,
+    #[arg(long, short, help = "Show the root folder at the top of the tree.")]
     pub root: bool,
+    #[arg(long, short, help = "Print this help output.")]
+    pub help: bool,
+    #[arg(
+        long,
+        short,
+        help = "Generate a configuration file in [PATH] respecting passed arguments."
+    )]
+    pub generate: bool,
 }
 
-impl From<ArgMatches> for Args {
-    fn from(matches: ArgMatches) -> Self {
-        Self {
-            depth: matches.get_one::<usize>("depth").copied().unwrap_or(0),
-            icons: matches
-                .get_one::<IconArg>("icons")
-                .copied()
-                .unwrap_or(IconArg::None)
-                .into(),
-            path: matches
-                .get_one::<PathBuf>("path")
-                .cloned()
-                .unwrap_or(PathBuf::from("./")),
-            all_files: matches.get_one::<bool>("all").copied().unwrap_or_default(),
-            root: matches.get_one::<bool>("root").copied().unwrap_or_default(),
-        }
+pub fn print_help_if_needed(args: Args) -> bool {
+    if args.help {
+        build_help_printer(Args::command()).print_help();
+        return true;
     }
+
+    false
 }
 
-pub fn build_cli() -> Command {
-    let gen_conf = command!("gen").about("Generate a .boughconf file with the passed options, filling out all found files and directories");
+static INTRODUCTION: &str = "
+**bough** is an overengineered CLI for creating file-tree diagrams in a variety of output formats and styles.
 
-    command!()
-        .subcommand(gen_conf)
-        .arg(
-            Arg::new("path")
-                .num_args(1)
-                .value_parser(value_parser!(PathBuf))
-                .default_value("./"),
-        )
-        .arg(
-            Arg::new("icons")
-                .value_name("style")
-                .short('i')
-                .long("icons")
-                .default_value("none")
-                .value_parser(value_parser!(IconArg))
-                .help("The style of icons to use"),
-        )
-        .arg(
-            Arg::new("depth")
-                .short('d')
-                .long("depth")
-                .value_parser(value_parser!(usize))
-                .default_value("0")
-                .help("The depth to build the tree to. 0 will build a tree as large as possible"),
-        )
-        .arg(
-            Arg::new("all")
-                .short('a')
-                .long("all")
-                .action(ArgAction::SetTrue)
-                .help("Show hidden files"),
-        )
-        .arg(
-            Arg::new("root")
-                .short('r')
-                .long("root")
-                .action(ArgAction::SetTrue)
-                .help("Show the root directory in the file tree"),
-        )
+It supports:
+* HTML, ANSI, raw text, and Markdown output.
+* Varying icon styles
+* Configuration files
+";
+
+static AFTER_HELP: &str = "
+See also: `bough-lib` and `boughd`!
+";
+
+static AUTHOR: &str = "*Made with ♥ by ${author}";
+
+static EXAMPLE_TEMPLATE: &str = "
+**Examples:**
+${examples
+**${example-number})** ${example-title}: `${example-cmd}`
+${example-comments}
+}
+";
+
+struct Example<'a> {
+    pub title: &'a str,
+    pub cmd: &'a str,
+    pub comments: &'a str,
+}
+
+static EXAMPLES: [Example<'static>; 1] = [Example {
+    title: "Build an HTML tree outputting it to a file",
+    cmd: "bough ./src -o output.html -i nerd",
+    comments: "This outputs an HTML tree based on `./src` with Nerd Font icons to `output.html`",
+}];
+
+fn build_help_printer(cmd: Command) -> Printer<'static> {
+    let mut p = Printer::new(cmd)
+        .without("author")
+        .with("introduction", INTRODUCTION);
+
+    p.template_keys_mut().push("examples");
+    p.template_keys_mut().push("after");
+    p.set_template("examples", EXAMPLE_TEMPLATE);
+    for (i, example) in EXAMPLES.iter().enumerate() {
+        p.expander_mut()
+            .sub("examples")
+            .set("example-number", i + 1)
+            .set("example-title", example.title)
+            .set("example-cmd", example.cmd)
+            .set_md("example-comments", example.comments);
+    }
+    p.template_keys_mut().push("author_custom");
+    p.set_template("author_custom", AUTHOR);
+    p.set_template("after", AFTER_HELP);
+
+    let skin = p.skin_mut();
+    skin.headers[0].set_fg(Color::AnsiValue(198));
+    skin.bold.set_fg(Color::AnsiValue(198));
+
+    p
 }
 
 pub fn get_matches(cmd: Command) -> ArgMatches {
--- bough/src/main.rs
+++ bough/src/main.rs
@@ -1,25 +1,13 @@
-use crate::{
-    cli::{build_cli, get_matches, Args},
-    files::{ignore_git, FileTree, IgnoreFiles},
-};
+use clap::Parser;
+
+use crate::cli::{print_help_if_needed, Args};
 
 mod cli;
-mod files;
 
 fn main() {
-    let cli = build_cli();
-    let matches = get_matches(cli);
+    let args = Args::parse();
 
-    let args = Args::from(matches.clone());
-
-    let mut ignore = IgnoreFiles {
-        hidden: !args.all_files,
-        paths: vec![],
-    };
-
-    ignore.paths.append(&mut ignore_git().unwrap());
-
-    let init_tree = FileTree::build(args.path, args.depth, ignore);
-
-    println!("{init_tree}");
+    if print_help_if_needed(args) {
+        return;
+    }
 }
